2cd019b1f0a87c02b5d5d451f01e8ef3cd3b2d3e
youssefamerzag <amerzag.youssef4@gmail.com>
2025-01-09 10:48:36 +0100

    add constants (collection , fields and services)

---
+++ src/main/java/com/example/rh/constants/Collections.java
@@ -0,0 +1,9 @@
+package com.example.rh.constants;
+
+public class Collections {
+  public static final String USER = "user";
+  public static final String CONTRACTS = "contracts";
+  public static final String DEMANDS = "demands";
+  public static final String FILES = "files";
+  public static final String NOTIFICATIONS = "notifications";
+}
---
+++ src/main/java/com/example/rh/constants/Services.java
@@ -0,0 +1,16 @@
+package com.example.rh.constants;
+
+public class Services {
+
+  //DB
+  public static final String DB_FIND = "find";
+  public static final String DB_FIND_ONE = "findOne";
+  public static final String DB_FIND_WITH_OPTIONS = "findWithOptions";
+  public static final String DB_INSERT = "insert";
+  public static final String DB_UPDATE = "update";
+  public static final String DB_UPDATE_COLLECTION_WITH_OPTIONS = "updateCollectionWithOptions";
+  public static final String DB_REMOVE_DOCUMENT = "removeDocument";
+  public static final String DB_REMOVE_DOCUMENTS = "removeDocuments";
+  public static final String DB_COUNT = "count";
+
+}
