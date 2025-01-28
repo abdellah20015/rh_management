eac2a098e6818c0a6f9f8b7f77ed0dde8a099742
ilyass-b <ilyass12345om@gmail.com>
2025-01-28 12:28:58 +0100

    fix create user fields.java authverticle

--- src/main/java/com/example/rh/services/AuthVerticle.java
+++ src/main/java/com/example/rh/services/AuthVerticle.java
@@ -149,6 +149,7 @@ public class AuthVerticle extends AbstractVerticle {
                     if (reply.result().body() == null) {
                         mongoUserUtil.createUser(username, password, res -> {
                             if (res.succeeded()) {
+                                System.out.println(body);
                                 JsonObject payload2 = new JsonObject()
                                         .put("collection", Collections.USER)
                                         .put("id", res.result())
@@ -163,6 +164,7 @@ public class AuthVerticle extends AbstractVerticle {
                                                 .put(Fields.USER_FIRSTNAME, body.getString(Fields.USER_FIRSTNAME ,null))
                                                 .put(Fields.USER_LASTNAME, body.getString(Fields.USER_LASTNAME ,null))
                                                 .put(Fields.USER_PHONE, body.getString(Fields.USER_PHONE ,null))
+                                                .put(Fields.USER_CIN, body.getString(Fields.USER_CIN ,null))
                                                 .put(Fields.USER_PERMISSIONS, permissions));
 
                                 vertx.eventBus().request(Services.DB_UPDATE, payload2, reply2 -> {
